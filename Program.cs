5d90a9f35e696373308e6f6d0f9547f70ca36c5b
26Kevin <113374266+26Kevin@users.noreply.github.com>
2023-06-09 11:23:08 -0600

    udpating

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Distance Calculator By Kevin
+#nullable disable
+
+// WELCOME
+Console.Clear();
+Console.WriteLine("WELCOME TO THE DISTANCE CALCULATOR!");
+
+Console.Write("Enter x1 value: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter y1 value: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter x2 value: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter y2 value: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+// Process
+double distance = 
+
+
+
