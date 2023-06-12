4b152e15ca5344824782907c178cdeaedf2130f5
26Kevin <113374266+26Kevin@users.noreply.github.com>
2023-06-12 14:09:29 -0600

    finished

--- Program.cs
+++ Program.cs
@@ -6,16 +6,20 @@ Console.Clear();
 Console.WriteLine("WELCOME TO THE DISTANCE CALCULATOR!");
 
 Console.Write("Enter x1 value: ");
-double x1 = Convert.ToDouble(Console.ReadLine());
+int x1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter y1 value: ");
-double y1 = Convert.ToDouble(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter x2 value: ");
-double x2 = Convert.ToDouble(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter y2 value: ");
-double y2 = Convert.ToDouble(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
 
 // Process
-double distance = 
+int distance = (int)Math.Sqrt((x2-x1) * (x2-x1)+ (y2-y1) * (y2-y1));
+
+// Output
+Console.WriteLine($"Distance between the two points is {distance}");
+
 
 
 
